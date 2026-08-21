e26353c6715eb0e1a54965be8cbeb55d3e90e6f3
ryan216chen <ryanchen216@gmail.com>
2026-08-22 00:14:02 +0800

    resume

--- scripts/training/resume_train.py
+++ scripts/training/resume_train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO 
 from pathlib import Path 
 
-LAST_MODEL = Path(r)
+LAST_MODEL = Path(r"D:\road-damage-detection\runs\road-segmentation\train_road_segmentation\weights\last.pt")
 
 def main():
 
